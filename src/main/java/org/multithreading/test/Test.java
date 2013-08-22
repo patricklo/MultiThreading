caba2146c214c446aeb20db45b6ff8eb49411bc3
patricklo <patrick.jh.lo@gmail.com>
2013-08-22 23:27:45 +0800

    project files

---
+++ src/main/java/org/multithreading/test/Test.java
@@ -0,0 +1,14 @@
+package org.multithreading.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: patricklo
+ * Date: 22/8/13
+ * Time: 11:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] arg){
+        System.out.print("Testing GitHub Multithreading");
+    }
+}
